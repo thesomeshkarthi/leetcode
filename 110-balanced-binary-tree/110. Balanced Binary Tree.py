44e119709853b7b8daa7d9e17bdf142cd0eb69da
Somesh Karthi <54777592+thesomeshkarthi@users.noreply.github.com>
2024-08-18 13:29:53 -0400

    Runtime: 45 ms (57.86%), Memory: 17.6 MB (54.39%) - Gitcode

---
+++ 110-balanced-binary-tree/110. Balanced Binary Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        self.balanced = True
+
+        def depth(node):
+            if not node:
+                return 0
+            left = depth(node.left)
+            right = depth(node.right)
+
+            if abs(left - right) > 1:
+                self.balanced = False
+
+            return 1 + max(left, right)
+        
+        depth(root)
+        return self.balanced
+
+
+
+        
