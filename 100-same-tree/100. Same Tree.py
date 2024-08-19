af5ab980d209906a64b112d8a8ab0f6ca5748c22
Somesh Karthi <54777592+thesomeshkarthi@users.noreply.github.com>
2024-08-19 10:50:04 -0400

    Runtime: 31 ms (80.93%), Memory: 16.5 MB (82.56%) - Gitcode

--- 100-same-tree/100. Same Tree.py
+++ 100-same-tree/100. Same Tree.py
@@ -6,36 +6,21 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
-        self.same = True
 
-        def dfs(curr1, curr2):
-            if curr1 is None:
-                if curr2 is None:
-                    return 0
-                if curr2:
-                    self.same = False
-                    return 0
-            if curr2 is None:
-                if curr1 is None:
-                    return 0
-                if curr1:
-                    self.same = False
-                    return 0
-            
-            dfs(curr1.right, curr2.right)
-            
-            if curr1.val != curr2.val:
-                self.same = False
+        if not p and not q:
+            return True
+        if p and q:
+            if p.val == q.val:
+                return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        else:
+            return False
 
-            dfs(curr1.left, curr2.left)
 
-            if curr1.val != curr2.val:
-                self.same = False
 
-            return 0
-        
-        dfs(p, q)
-        return self.same
+
+
+
+
+
 
         
