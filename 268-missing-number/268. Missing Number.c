ebb4aed28a0ec2f4be167d94ab54e2936413e53a
Somesh Karthi <54777592+thesomeshkarthi@users.noreply.github.com>
2025-01-04 15:14:43 -0500

    Runtime: 0 ms (100.00%), Memory: 9 MB (28.44%) - Gitcode

--- 268-missing-number/268. Missing Number.c
+++ 268-missing-number/268. Missing Number.c
@@ -1,16 +1,14 @@
 int missingNumber(int* nums, int numsSize) {
-    int numberArr[numsSize + 1];
     int res = 0;
     for (int i = 0; i < numsSize + 1; i++){
-        numberArr[i] = i;
 
         if(i == numsSize){
-            res ^= numberArr[i];
+            res ^= i;
             break;
         }
 
         res ^= nums[i]; 
-        res ^= numberArr[i];
+        res ^= i;
     }
 
     return res;   
