dc72c9d4607461cbc6654c05d54722ab15ed4051
Somesh Karthi <54777592+thesomeshkarthi@users.noreply.github.com>
2025-01-04 14:46:11 -0500

    Runtime: 5 ms (3.99%), Memory: 7.7 MB (16.93%) - Gitcode

--- 190-reverse-bits/190. Reverse Bits.c
+++ 190-reverse-bits/190. Reverse Bits.c
@@ -3,10 +3,10 @@ uint32_t reverseBits(uint32_t n) {
     uint32_t res = 0;
 
     for (int i = 0; i < 32; i++){
-        res = res << 1;
+        res <<=  1;
         int lsb = n & 1;
         res |= lsb;
-        n = n >> 1;
+        n >>= 1;
     }
 
     return res; 
